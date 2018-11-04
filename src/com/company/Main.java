8193f1d5b54c9cf40c6f6a3679a541ed1051209d
anwar <anwar.lynch@gmail.com>
2018-11-04 11:43:47 -0600

    Reverted calculation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        printYearsAndDays(-1); //return invalid value
-//        printYearsAndDays(525600); //return 1y and 0d
-//        printYearsAndDays(1051200); //return 2y and 0d
-        printYearsAndDays(1051200); //return 1y and 25d
+        printYearsAndDays(-1); //return invalid value
+        printYearsAndDays(525600); //return 1y and 0d
+        printYearsAndDays(1051200); //return 2y and 0d
+        printYearsAndDays(561600); //return 2y and 0d
+        printYearsAndDays(0); //return 0y and 0d
     }
 
     public static void printYearsAndDays(long minutes) {
@@ -15,11 +16,10 @@ public class Main {
 
         } else {
 
-            long hours = minutes/60;
-            long days = hours/24;
-            long years = days/365;
-            long remainingDays = days % 365;
-            System.out.println(minutes + " min = " +years +" y and " + remainingDays + " d");
+            long years = minutes / 525600;
+            long remainingMinutes = minutes % 525600;
+            long days = remainingMinutes/1440;
+            System.out.println(minutes + " min = " +years +" y and " + days + " d");
 
         }
     }
